68ffd281f4874cccc29a4eff8ac8aff2ddc702ff
Emil Slavov <easlavov@gmail.com>
2019-11-24 22:17:22 +0200

    playing

--- playground.rb
+++ playground.rb
@@ -1,14 +1,3 @@
-3.times { |time|
-  puts time
-}
-
-def count_elements(arr)
-  arr.length
-end
-
-arr1 = [1,2,4, 10]
-
-puts count_elements arr1
-str = "sample string"
-puts str.upcase
-puts "emo".reverse.upcase
+var = { :one => "eins" }
+var2 = {one: "eins"}
+puts var == var2
